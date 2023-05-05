ab32d108dea57d996eb1af4751dbd88b895459af
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-05 12:16:50 +0300

    express server started listening to the appropriate port

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager App.");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
