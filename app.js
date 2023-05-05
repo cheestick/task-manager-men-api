a2a9a4c50bccf0bc11abccf5a7ccf833ccc3cdd0
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-05 12:52:40 +0300

    app.js was improved by separating parts such as tasks router, constant utility. Express.json middleware was also plugged in

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+const taskRouter = require("./routes/tasks");
+const { TASK_API_URL } = require("./utils/const");
 
-//routes
+// middleware
+app.use(express.json());
+
+// routes
 app.get("/hello", (req, res) => {
   res.send("Task manager App.");
 });
 
+app.use(TASK_API_URL, taskRouter);
+
+// server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
