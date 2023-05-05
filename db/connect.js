06e8e3129ad539b2ad38b51cb0c02d31ebe4271e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-05 17:21:35 +0300

    connection to db function was refactored

--- db/connect.js
+++ db/connect.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-const { ATLAS_USER, ATLAS_PSWD } = process.env;
-
-const connectionString = `mongodb+srv://${ATLAS_USER}:${ATLAS_PSWD}@nodeexpressproject.npb2ppk.mongodb.net/task-manager-men?retryWrites=true&w=majority`;
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("CONNECTED TO THE DB..."))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
